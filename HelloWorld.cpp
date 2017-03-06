7a71b0f83636fc6f25b525941abe080afb932779
fla07 <flavitta007@gmail.com>
2017-03-06 11:48:00 -0300

    A simple Hello World example

---
+++ HelloWorld.cpp
@@ -0,0 +1,27 @@
+/*
+ Hello, dudes! I'm Flavia Pino, and this is our first tutorial, and today we are 
+ going to learn the basic concepts about C++
+ I'm using Visual Studio, but you can use Dev C++, Net Beans or other editor.
+ 
+ HelloWorld.cpp : Defines the entry point for the console application
+
+ PS: this is a long comment ;)
+*/
+
+// And this is a short comment =D
+
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+	//when we want to print information on scream, we use "cout"	
+	cout << "Hello World, how nice is living in a blue little planet!"<<endl;
+
+	return 0;
+}
+
